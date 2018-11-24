c85246c2f4a10f08325318e5d08065babe18955c
Amel AZZI <ea_azzi@esi.dz>
2018-11-24 22:08:01 +0200

    add graph test with static data

---
+++ app/src/main/java/com/example/vladsobol/bavus/GraphActivity.java
@@ -0,0 +1,28 @@
+package com.example.vladsobol.bavus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fleetboard.sdk.lib.android.log.Log;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class GraphActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+    }
+}
