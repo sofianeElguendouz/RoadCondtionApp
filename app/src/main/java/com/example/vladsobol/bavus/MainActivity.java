8562d8b13a9b6a834bda2e4ccd48ea74ba15fc41
Vlad Sobol <vladsobol@dhcp-86-212.eduroam.aalto.fi>
2018-11-24 18:06:18 +0200

    first commit

---
+++ app/src/main/java/com/example/vladsobol/bavus/MainActivity.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2011-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.vladsobol.bavus;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
+
+/**
+ * Main activity which launches map view and handles Android run-time requesting permission.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+    private MapFragmentView m_mapFragmentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+        requestPermissions();
+
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    /**
+     * Only when the app's target SDK is 23 or higher, it requests each dangerous permissions it
+     * needs when the app is running.
+     */
+    private void requestPermissions() {
+
+        String[] RUNTIME_PERMISSIONS = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE};
+
+        // check list of permissions
+        boolean isPermissionsGranted = true;
+        for (String permission : RUNTIME_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                isPermissionsGranted = false;
+                break;
+            }
+        }
+
+        // return if all permissions already granted
+        if (isPermissionsGranted) {
+            createMapFragmentView();
+            return;
+        }
+
+        // request permissions
+        if (android.os.Build.VERSION.SDK_INT >= 23){
+            ActivityCompat.requestPermissions(this,
+                    RUNTIME_PERMISSIONS,
+                    REQUEST_CODE_ASK_PERMISSIONS);
+
+        }
+
+    }
+
+
+    /**
+     * Create map fragment view.
+     * !!! Please note: the HERE SDK requires all permissions defined above to operate properly. !!!
+     */
+    void createMapFragmentView() {
+        m_mapFragmentView = new MapFragmentView(this);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS: {
+                for (int index = 0; index < permissions.length; index++) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+
+                        /**
+                         * If the user turned down the permission request in the past and chose the
+                         * Don't ask again option in the permission request system dialog.
+                         */
+                        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                permissions[index])) {
+                            Toast.makeText(this,
+                                    "Required permission " + permissions[index] + " not granted. "
+                                            + "Please go to settings and turn on for sample app",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this,
+                                    "Required permission " + permissions[index] + " not granted",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+                /*
+                 * All permission requests are being handled. Create map fragment view.
+                 */
+                createMapFragmentView();
+                break;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/vladsobol/bavus/MapFragmentView.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2011-2018 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.vladsobol.bavus;
+
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.GeoPolyline;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+import com.here.android.mpa.mapping.MapPolyline;
+
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class encapsulates the properties and functionality of the Map view.
+ */
+public class MapFragmentView {
+    private MapFragment m_mapFragment;
+    private Activity m_activity;
+    private Map m_map;
+
+    public MapFragmentView(Activity activity) {
+        m_activity = activity;
+        initMapFragment();
+    }
+
+    // Google has deprecated android.app.Fragment class. It is used in current SDK implementation.
+    // Will be fixed in future SDK version.
+    @SuppressWarnings("deprecation")
+    private MapFragment getMapFragment() {
+        return (MapFragment) m_activity.getFragmentManager().findFragmentById(R.id.mapfragment);
+    }
+
+    private int ratingToColor(double rating) {
+        int alpha = 255;
+        int blue = 0;
+        int green = Math.round(((1 << 8) - 1) * (float)(1 - rating));
+        int red = Math.round(((1 << 8) - 1) * (float)(rating));
+        int result = (alpha << 24) + (red << 16) + (green << 8) + blue;
+        return result;
+
+    }
+
+    private void drawLine(MapFragment map, double lat1, double lon1, double lat2, double lon2, double rating) {
+        List<GeoCoordinate> testPoints = new ArrayList<GeoCoordinate>();
+        testPoints.add(new GeoCoordinate(lat1, lon1, 10));
+        testPoints.add(new GeoCoordinate(lat2, lon2, 10));
+        GeoPolyline polyline = new GeoPolyline(testPoints);
+        MapPolyline mapPolyline = new MapPolyline(polyline);
+        mapPolyline.setLineColor(ratingToColor(rating));
+        mapPolyline.setLineWidth(10);
+        mapPolyline.setZIndex(100);
+        map.getMap().addMapObject(mapPolyline);
+    }
+
+    private void initMapFragment() {
+        /* Locate the mapFragment UI element */
+        m_mapFragment = getMapFragment();
+
+
+
+        // Set path of isolated disk cache
+        String diskCacheRoot = Environment.getExternalStorageDirectory().getPath()
+                + File.separator + ".isolated-here-maps";
+        // Retrieve intent name from manifest
+        String intentName = "";
+        try {
+            ApplicationInfo ai = m_activity.getPackageManager().getApplicationInfo(m_activity.getPackageName(),
+                    PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            intentName = "NAME";
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(this.getClass().toString(), "Failed to find intent name, NameNotFound: " + e.getMessage());
+        }
+
+        // Some check was done here before, not sure what's happening.
+        if (m_mapFragment != null) {
+        /* Initialize the MapFragment, results will be given via the called back. */
+            m_mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(Error error) {
+
+                    if (error == Error.NONE) {
+                    /*
+                     * If no error returned from map fragment initialization, the map will be
+                     * rendered on screen at this moment.Further actions on map can be provided
+                     * by calling Map APIs.
+                     */
+                        m_map = m_mapFragment.getMap();
+
+                        /*
+                         * Map center can be set to a desired location at this point.
+                         * It also can be set to the current location ,which needs to be delivered by the PositioningManager.
+                         * Please refer to the user guide for how to get the real-time location.
+                         */
+
+                        m_map.setCenter(new GeoCoordinate(
+                                60.184881, 24.832714), Map.Animation.NONE);
+                        m_map.setZoomLevel(16);
+                        PositioningManager mPositioningManager = PositioningManager.getInstance();
+                        mPositioningManager.start(PositioningManager.LocationMethod.GPS);
+                        m_map.getPositionIndicator().setVisible(true);
+                    } else {
+                        Toast.makeText(m_activity,
+                                "ERROR: Cannot initialize Map with error " + error,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+
+                    drawLine(m_mapFragment, 60.184881, 24.832714, 60.1866719, 25.8254933, 0.7);
+
+
+                    /*try {
+
+                        InputStreamReader is = new InputStreamReader(m_mapFragment
+                                .getActivity()
+                                .getAssets()
+                                .open("filename.csv"));
+
+                        BufferedReader reader = new BufferedReader(is);
+
+                        reader.readLine();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+
+                        }
+                    } catch (Exception e)
+                    {
+                        // nothing
+                    }
+                    */
+                }
+
+
+            });
+        }
+    }
+}
